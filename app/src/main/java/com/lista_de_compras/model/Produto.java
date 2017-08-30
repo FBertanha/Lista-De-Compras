dd5cdcd58f80a11b81b321e3379f1c27c27517f1
Evelyn Sanches <evelyn.eps@hotmail.com>
2017-08-30 14:24:37 -0300

    Criação das 4 classes

--- app/src/main/java/com/lista_de_compras/model/Produto.java
+++ app/src/main/java/com/lista_de_compras/model/Produto.java
@@ -1,11 +1,8 @@
 package com.lista_de_compras.model;
 
-import java.util.List;
-
 /**
  * Created by evely on 30/08/2017.
  */
 
-public class Lista {
-    List<Produto> produtos;
+public class Produto {
 }
