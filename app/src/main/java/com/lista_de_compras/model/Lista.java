fd630fdb03388917d7c5b5db8ad59c0aebb71481
Evelyn Sanches <evelyn.eps@hotmail.com>
2017-09-13 13:41:02 -0300

    Criação dos gets e sets

--- app/src/main/java/com/lista_de_compras/model/Lista.java
+++ app/src/main/java/com/lista_de_compras/model/Lista.java
@@ -8,10 +8,58 @@ import java.util.List;
  */
 
 public class Lista {
-    int codigo;
-    CategoriaDeLista categoria;
-    List<Produto> produtos;
-    String nome;
-    Date dataCriacao;
-    Date dataCompra;
+    private int codigo;
+    private CategoriaDeLista categoria;
+    private List<Produto> produtos;
+    private String nome;
+    private Date dataCriacao;
+    private Date dataCompra;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public CategoriaDeLista getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaDeLista categoria) {
+        this.categoria = categoria;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Date getDataCompra() {
+        return dataCompra;
+    }
+
+    public void setDataCompra(Date dataCompra) {
+        this.dataCompra = dataCompra;
+    }
 }
--- app/src/main/java/com/lista_de_compras/model/Produto.java
+++ app/src/main/java/com/lista_de_compras/model/Produto.java
@@ -5,8 +5,40 @@ package com.lista_de_compras.model;
  */
 
 public class Produto {
-    int codigo;
-    String nome;
-    CategoriaDeProduto categoria;
-    double valor;
+    private int codigo;
+    private String nome;
+    private CategoriaDeProduto categoria;
+    private double valor;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public CategoriaDeProduto getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaDeProduto categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 }
